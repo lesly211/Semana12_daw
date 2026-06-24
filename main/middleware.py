49f3a0774cc306b177ec8ad3a88f97a2a4eb192a
Lesly Brenda Navarro Serva <leslynavarro804@gmail.com>
2026-06-24 13:32:24 -0500

    PASO5

--- main/middleware.py
+++ main/middleware.py
@@ -3,6 +3,8 @@ class SimpleMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        print(f"[LOG MIDDLEWARE] {request.method} - {request.path}")
         response = self.get_response(request)
         response['X-Custom-Header'] = 'Semana12-DAW'
+        response['Content-Security-Policy'] = "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src https://fonts.gstatic.com;"
         return response
